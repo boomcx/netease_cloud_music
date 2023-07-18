cc4215baad5867c46140798071cdbf168baa1710
tonga <707939932@qq.com>
2023-07-18 11:47:40 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/boomcx/netease_cloud_music/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.boomcx.netease_cloud_music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home/views/home_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../providers/home_provider.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count1 = ref.watch(homeProvider.select((value) => value.count));
+    final count2 = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => ref.read(homeProvider.notifier).increment(),
+              child: Text('use state: $count1'),
+            ),
+            ElevatedButton(
+              onPressed: () => ref.read(counterProvider.notifier).increment(),
+              child: Text(
+                'single provider: $count2',
+                style: TextStyle(
+                  fontFamily: 'Dolpgin',
+                  // fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
